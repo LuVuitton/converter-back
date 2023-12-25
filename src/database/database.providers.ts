c16cde52436bc3eca469f944df0d05be0e22712d
FelixT <felixtekhada@gmail.com>
2023-12-25 02:30:49 +0100

    changed conf

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -7,7 +7,7 @@ export const databaseProviders = [
       const dataSource = new DataSource({
         type: 'mysql',
         host: process.env.DATABASE_HOST,
-        port: +process.env.DATABASE_PORT,
+        // port: +process.env.DATABASE_PORT,
         username: process.env.DATABASE_USERNAME,
         password: process.env.DATABASE_PASSWORD,
         database: process.env.DATABASE_NAME,
